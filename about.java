3726347eb168118a52f8bfd2c63fc99c16dd9e03
dhanajee <dhanajee@yahoo.com>
2018-07-19 20:14:35 +0400

    About File - Need Review

--- about.java
+++ about.java
@@ -6,7 +6,7 @@ public class about
 
 	public about()
 	{
-		JFrame frame=new JFrame("About");
+		JFrame frame=new JFrame("About MyNotepad");
 
 		frame.add(new TextArea("Menu Exp\njdk-8u162-windows-x64\n©Dhanajee J..All rights reserved\n31 March 2018"));
 		frame.setVisible(true);
